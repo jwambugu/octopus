08cfeb0deb8f57d91e1d86d76654ca26c2e900ff
Joram Wambugu <artisan254@gmail.com>
2021-03-02 15:28:36 +0300

    Payments
    Create the view for confirming mpesa payments.

--- app/Http/Controllers/API/MpesaController.php
+++ app/Http/Controllers/API/MpesaController.php
@@ -3,9 +3,73 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MpesaController extends Controller
 {
-    //
+    /**
+     * Process the callback data sent by mpesa
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function lipaNaMpesaCallback(Request $request)
+    {
+        $response = json_decode($request->getContent());
+
+        info('mpesa-response', [$response]);
+
+//        $callbackData = $response->Body->stkCallback;
+//        $merchantRequestID = $callbackData->MerchantRequestID;
+//        $checkoutRequestID = $callbackData->CheckoutRequestID;
+//        $resultCode = $callbackData->ResultCode;
+//
+//        // Validate the transaction
+//        $payment = $this->validateTransaction($merchantRequestID, $checkoutRequestID);
+//
+//        if (!$payment) {
+//            return $this->rejectTransaction();
+//        }
+//
+//        // Check if the transaction was successful. All successful transactions have a result code of 0
+//        if ($resultCode != 0) {
+//            // Mark the transaction as failed
+//            try {
+//                $this->updateTransactionData($payment, [
+//                    'is_paid' => false,
+//                    'callback_data' => $response,
+//                    'transaction_id' => null,
+//                    'paid_amount' => 0
+//                ]);
+//            } catch (Exception $e) {
+//                return $this->rejectTransaction();
+//            }
+//
+//            return $this->acceptTransaction();
+//        }
+//
+//        // Extract the request metadata
+//        $callbackMetadata = $callbackData->CallbackMetadata;
+//        $transactionID = $callbackMetadata->Item[1]->Value;
+//        $amount = $callbackMetadata->Item[0]->Value;
+//
+//        // Check if we have a similar transaction id
+//        if ($this->transactionIDExists($transactionID)) {
+//            return $this->rejectTransaction();
+//        }
+//
+//        try {
+//            // Mark the transaction as successful
+//            $this->updateTransactionData($payment, [
+//                'is_paid' => true,
+//                'callback_data' => $response,
+//                'transaction_id' => $transactionID,
+//                'paid_amount' => $amount
+//            ]);
+//        } catch (Exception $e) {
+//            return $this->rejectTransaction();
+//        }
+//
+//        return $this->acceptTransaction();
+    }
 }
--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,38 +3,43 @@
 namespace App\Models;
 
 use Eloquent;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Carbon;
+
 
 /**
  * App\Models\Booking
  *
  * @property int $id
+ * @property string $uuid
  * @property string $checkin_date
  * @property string $checkout_date
  * @property int $property_id
  * @property int $user_id
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * @property string|null $deleted_at
- * @method static Builder|Booking newModelQuery()
- * @method static Builder|Booking newQuery()
- * @method static Builder|Booking query()
- * @method static Builder|Booking whereCheckinDate($value)
- * @method static Builder|Booking whereCheckoutDate($value)
- * @method static Builder|Booking whereCreatedAt($value)
- * @method static Builder|Booking whereDeletedAt($value)
- * @method static Builder|Booking whereId($value)
- * @method static Builder|Booking wherePropertyId($value)
- * @method static Builder|Booking whereUpdatedAt($value)
- * @method static Builder|Booking whereUserId($value)
+ * @property Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking newQuery()
+ * @method static Builder|Booking onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereCheckinDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereCheckoutDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking wherePropertyId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Booking whereUuid($value)
+ * @method static Builder|Booking withTrashed()
+ * @method static Builder|Booking withoutTrashed()
  * @mixin Eloquent
- * @method static \Illuminate\Database\Query\Builder|Booking onlyTrashed()
- * @method static \Illuminate\Database\Query\Builder|Booking withTrashed()
- * @method static \Illuminate\Database\Query\Builder|Booking withoutTrashed()
  */
 class Booking extends Model
 {
@@ -46,9 +51,41 @@ class Booking extends Model
      * @var array
      */
     protected $fillable = [
+        'uuid',
         'checkin_date',
         'checkout_date',
         'property_id',
         'user_id',
     ];
+
+    /**
+     * Returns the booking payments
+     * @return HasMany
+     */
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    /**
+     * Returns the property being booked
+     * @return BelongsTo
+     */
+    public function property(): BelongsTo
+    {
+        return $this->belongsTo(Property::class, 'property_id')->select([
+            'id', 'name', 'slug', 'cost_per_night'
+        ]);
+    }
+
+    /**
+     * Returns the payments that were unsuccessful
+     * @return HasMany
+     */
+    public function unsuccessfulPayments(): HasMany
+    {
+        return $this->payments()->where('is_successful', false)->select([
+            'id', 'uuid', 'account_number', 'amount', 'booking_id'
+        ]);
+    }
 }
