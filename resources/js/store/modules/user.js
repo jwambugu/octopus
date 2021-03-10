dd5dc6f4f9c5e342b5688c50c82e15121ea13247
Joram Wambugu <artisan254@gmail.com>
2021-03-10 15:17:51 +0300

    add compiled assets

---
+++ resources/js/store/modules/user.js
@@ -0,0 +1,60 @@
+import { errorHandler } from "../../utils";
+
+const state = {};
+
+const getters = {};
+
+const mutations = {};
+
+const actions = {
+    BOOK_PROPERTY: ({}, payload) => {
+        return new Promise((resolve, reject) => {
+            const { checkin_date, checkout_date, property_id } = payload;
+
+            axios
+                .post("/bookings/book-property", {
+                    checkin_date,
+                    checkout_date,
+                    property_id,
+                })
+                .then(({ data }) => {
+                    resolve(data.data);
+                })
+                .catch((error) => reject(errorHandler(error)));
+        });
+    },
+    MAKE_MPESA_PAYMENT: ({}, payload) => {
+        return new Promise((resolve, reject) => {
+            const { phoneNumber, paymentID } = payload;
+
+            axios
+                .post("/bookings/lipa-na-mpesa", {
+                    phone_number: phoneNumber,
+                    payment_id: paymentID,
+                })
+                .then(({ data }) => {
+                    resolve(data.data);
+                })
+                .catch((error) => reject(errorHandler(error)));
+        });
+    },
+    CONFIRM_MPESA_PAYMENT: ({}, payload) => {
+        return new Promise((resolve, reject) => {
+            const { uuid } = payload;
+
+            axios
+                .post(`/bookings/${uuid}/confirm-mpesa-payment`)
+                .then(({ data }) => {
+                    resolve(data.data);
+                })
+                .catch((error) => reject(errorHandler(error)));
+        });
+    },
+};
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions,
+};
