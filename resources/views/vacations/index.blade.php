83eafbe53ad52a71c33848aa52d1414b93560a23
Joram Wambugu <artisan254@gmail.com>
2021-06-16 12:20:36 +0300

    Add banner for M-Pesa payments.

--- resources/views/vacations/index.blade.php
+++ resources/views/vacations/index.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert alert-warning override-text-transform" style="font-size: 1.8rem;"
+                     role="alert">
+                    <span style="margin-left: 145px; font-weight: bold; color: #000000;">
+                       <i class="fa fa-info-circle text-warning"></i> Book your vacation and pay with M-Pesa.
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <properties-list-main-component style="margin-top: -40px" :page="{{ $page }}"
                                     :filters="{{ json_encode($filters) }}"
                                     :query-params="{{ json_encode($queryParams) }}"
